cc8b9815f22e4c43c02a262a4ef8d5bcc4e5473d
陈志谦 <761208@nd.com>
2020-04-15 17:53:35 +0800

    首次提交

---
+++ typings.d.ts
@@ -0,0 +1,25 @@
+
+declare var adhoc_cast_connection_console : {
+    log(...args: any[])
+    warn(...args: any[])
+    dir(...args: any[])
+    trace(...args: any[])
+    error(...args: any[])
+}
+
+declare var global: any;
+declare var IsNode: boolean;
+
+
+
+interface Window {
+    mynode: typeof mynode;
+}
+
+declare namespace mynode {
+    var electron: Electron.MainInterface;
+    var process: NodeJS.Process;
+    var fs: any;
+    var path: any;
+    var robotjs: any;
+}
---
+++ webpack.config.js
@@ -0,0 +1,2 @@
+if (process.env.NODE_MODULE === 'mdm-receiver') 
+    module.exports = require('./webpack/webpack.config.mdm.receiver.js');
